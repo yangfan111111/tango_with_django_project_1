35e3d8fa4d52203d9e7764ed2998b60e214c9306
fan yang <2359448y@student.gla.ac.uk>
2019-01-22 16:39:16 +0000

    chapter 5 finish

--- populate_rango.py
+++ populate_rango.py
@@ -8,30 +8,38 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url": "http://docs.python.org/2/tutorial/"},
+         "url": "http://docs.python.org/2/tutorial/",
+         "views": 32},
         {"title": "How to Think like a Computer Scientist",
-         "url": "http://www.greenteapress.com/thinkpython/"},
+         "url": "http://www.greenteapress.com/thinkpython/",
+         "views": 16},
         {"title": "Learn Python in 10 Minutes",
-         "url": "http://www.korokithakis.net/tutorials/python/"}]
+         "url": "http://www.korokithakis.net/tutorials/python/", "views": 8}]
     django_pages = [
         {"title": "Official Django Tutorial",
-         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 32},
         {"title": "Django Rocks",
-         "url": "http://www.djangorocks.com/"},
+         "url": "http://www.djangorocks.com/",
+         "views": 16},
         {"title": "How to Tango with Django",
-         "url": "http://www.tangowithdjango.com/"}]
+         "url": "http://www.tangowithdjango.com/",
+         "views": 8}]
     other_pages = [
         {"title": "Bottle",
-         "url": "http://bottlepy.org/docs/dev/"},
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 32},
         {"title": "Flask",
-         "url": "http://flask.pocoo.org"}]
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+         "url": "http://flask.pocoo.org",
+         "views": 16}]
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
+    
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
          for p in Page.objects.filter(category=c):
@@ -44,8 +52,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
      c = Category.objects.get_or_create(name=name)[0]
+     c.views = views
+     c.likes = likes
      c.save()
      return c
     
--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -22,3 +25,6 @@ class Page(models.Model):
     
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
+
+
+    
